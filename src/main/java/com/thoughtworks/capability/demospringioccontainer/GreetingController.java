291e934186b9c51f49e56c93ae334045f8443b5c
Lu-May <1054998345@qq.com>
2020-11-08 17:07:39 +0800

    [luyuehuan] feat:problem 2 - method  2

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
@@ -1,22 +1,31 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Lookup;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.naming.spi.ObjectFactory;
+
+import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_SINGLETON;
+
 @RestController
+@Scope(SCOPE_SINGLETON)
 public class GreetingController {
 
-    private final GreetingService greetingService;
-
     @Autowired
-    public GreetingController(GreetingService greetingService) {
-        this.greetingService = greetingService;
-    }
+    private ApplicationContext context;
 
     @GetMapping("/greet")
     public String greet() {
+        GreetingService greetingService = getFromApplicationContext();
         return greetingService.sayHi();
+    }
+
+    GreetingService getFromApplicationContext() {
+        return this.context.getBean("prototypeBean", GreetingService.class);
     }
 
 }
